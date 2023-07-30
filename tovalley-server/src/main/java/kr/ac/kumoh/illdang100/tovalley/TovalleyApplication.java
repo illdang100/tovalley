fa0353bcfbf7a6ff04d939cb2a4b97e142c58780
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-07-30 14:51:26 +0900

    spring-server init

---
+++ tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/TovalleyApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.illdang100.tovalley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TovalleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TovalleyApplication.class, args);
+	}
+
+}
---
+++ tovalley-server/src/test/java/kr/ac/kumoh/illdang100/tovalley/TovalleyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.illdang100.tovalley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TovalleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
