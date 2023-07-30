c747ffacca58587aefed228bc6500fcc68ded49c
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-07-31 00:55:43 +0900

    Feat: Create Member

---
+++ tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/domain/member/MemberRepository.java
@@ -0,0 +1,6 @@
+package kr.ac.kumoh.illdang100.tovalley.domain.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
