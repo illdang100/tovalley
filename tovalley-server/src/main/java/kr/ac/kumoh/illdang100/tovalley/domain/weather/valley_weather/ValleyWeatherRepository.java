91c8e6046207e00c9b2c941f3240a78af5d22ac4
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-07-31 00:57:04 +0900

    Feat: Create ValleyWeather

---
+++ tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/domain/weather/valley_weather/ValleyWeatherRepository.java
@@ -0,0 +1,6 @@
+package kr.ac.kumoh.illdang100.tovalley.domain.weather.valley_weather;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ValleyWeatherRepository extends JpaRepository<ValleyWeather, Long> {
+}
