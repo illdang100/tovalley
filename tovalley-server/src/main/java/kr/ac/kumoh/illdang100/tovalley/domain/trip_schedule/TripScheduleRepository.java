6061dbb0e9ffdceb8576328887e40ff6eb0276d6
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-07-31 00:58:20 +0900

    Feature: Create TripSchedule

---
+++ tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/domain/trip_schedule/TripScheduleRepository.java
@@ -0,0 +1,6 @@
+package kr.ac.kumoh.illdang100.tovalley.domain.trip_schedule;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TripScheduleRepository extends JpaRepository<TripSchedule, Long> {
+}
