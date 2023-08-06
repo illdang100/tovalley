b3f9048c1f638908fc3f302cbdd7fcd27c634147
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-08-07 00:52:50 +0900

    Feat: MainPageDto 생성

---
+++ tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/dto/MainPageRespDto.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.illdang100.tovalley.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+public class MainPageRespDto {
+
+    @AllArgsConstructor
+    @Data
+    public static class MainPageAllRespDto {
+
+    }
+}
