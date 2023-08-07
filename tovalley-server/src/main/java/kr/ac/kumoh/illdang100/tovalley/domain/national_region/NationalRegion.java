5f4d10c1ceb4d5a41af5e26c5ac153d89ff34cc8
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-08-08 02:09:23 +0900

    Feat: NationalRegion 엔티티 생성

---
+++ tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/domain/national_region/NationalRegion.java
@@ -0,0 +1,25 @@
+package kr.ac.kumoh.illdang100.tovalley.domain.national_region;
+
+import kr.ac.kumoh.illdang100.tovalley.domain.Coordinate;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class NationalRegion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "national_region_id")
+    private Long id;
+
+    @Column(nullable = false, length = 6)
+    private String regionName;
+
+    @Embedded
+    private Coordinate coordinate;
+}
