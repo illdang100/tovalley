8b38115f28578e7908c8a739bac1b13e6223bbf4
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-08-09 23:06:44 +0900

    Feat: openapi를 사용한 메서드 구현을 다른 서비스로 옮김

--- tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/service/WeatherService.java
+++ tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/service/WeatherService.java
@@ -7,25 +7,6 @@ import static kr.ac.kumoh.illdang100.tovalley.dto.weather.WeatherRespDto.*;
 public interface WeatherService {
 
     /**
-     * 전국 몇몇 지역의 날씨 정보를 Open API로부터 가져와 데이터베이스에 저장
-     * 백령, 서울, 춘천, 강릉, 수원, 청주, 울릉/독도, 대전, 안동, 전주, 대구, 울산, 광주, 목포, 부산, 제주, 여수
-     * 이 메서드는 일정 시간마다 주기적으로 실행된다.
-     */
-    void fetchAndSaveNationalWeatherData();
-
-    /**
-     * 전국 특보 정보를 Open API로부터 가져와 데이터베이스에 저장
-     * 이 메서드는 일정 시간마다 주기적으로 실행된다.
-     */
-    void fetchAndSaveSpecialWeatherData();
-
-    /**
-     * 전국 물놀이 지역 날씨를 Open API로부터 가져와 데이터베이스에 저장
-     * 이 메서드는 일정 시간마다 주기적으로 실행된다.
-     */
-    void fetchAndSaveAllWaterPlaceWeatherData();
-
-    /**
      * 전국 몇몇 지역의 현재 날씨 정보를 조회
      * 백령, 서울, 춘천, 강릉, 수원, 청주, 울릉/독도, 대전, 안동, 전주, 대구, 울산, 광주, 목포, 부산, 제주, 여수
      * @return 현재 날씨 정보
