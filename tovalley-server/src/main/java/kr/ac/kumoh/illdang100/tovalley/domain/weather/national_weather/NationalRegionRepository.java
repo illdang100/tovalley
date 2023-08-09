ecc9f7d1890e3a2aba9597bd04a4a0c5026a4a81
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-08-09 22:18:51 +0900

    Feat: NationalRegionRepository 생성

---
+++ tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/domain/weather/national_weather/NationalRegionRepository.java
@@ -0,0 +1,6 @@
+package kr.ac.kumoh.illdang100.tovalley.domain.weather.national_weather;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NationalRegionRepository extends JpaRepository<NationalRegion, Long> {
+}
