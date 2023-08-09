b7454a9a4618a30f49fe4a83d81981ed849f6a7f
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-08-09 13:10:09 +0900

    Feat: WaterPlaceDetail 생성

---
+++ tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/domain/waterplace/WaterPlaceDetail.java
@@ -0,0 +1,43 @@
+package kr.ac.kumoh.illdang100.tovalley.domain.waterplace;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class WaterPlaceDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "water_place_detail_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "water_place_id", nullable = false)
+    private WaterPlace waterPlace;
+
+    @Column(nullable = false, length = 20)
+    private String waterPlaySegment; // 물놀이구간(m)
+
+    @Column(nullable = false, length = 20)
+    private String deepestDepth; // 수심(깊은곳)
+
+    @Column(nullable = false, length = 20)
+    private String avgDepth; // 평균 수심
+
+    @Column(length = 20)
+    private String annualVisitors; // 연평균 총 이용객수(천명)
+
+    @Column(length = 20)
+    private String dangerSegments; // 위험구역구간
+
+    @Column(length = 20)
+    private String dangerSignboardsNum; // 위험구역 설정 안내표지판(합계)
+
+    @Column(length = 254)
+    private String safetyMeasures; // 안전조치 사항
+}
