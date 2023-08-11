506fe579fb69b391a55f44a232265a5784fc0eb1
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-08-11 17:46:56 +0900

    Update: ReviewRepository 내부 메서드 삭제

--- tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/domain/review/ReviewRepository.java
+++ tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/domain/review/ReviewRepository.java
@@ -1,11 +1,6 @@
 package kr.ac.kumoh.illdang100.tovalley.domain.review;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-
-    @Query("select COUNT(r) from Review r where r.waterPlace.id = :wId")
-    int findReviewCnt(@Param("wId") Long waterPlaceId);
 }
