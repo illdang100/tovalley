11f096a1695acce30d503d176e1ce90e59322a66
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-08-16 00:30:41 +0900

    Feat: 물놀이 장소 상세 정보 조회 기능 구현
    물놀이 장소 정보 + 구조용품 정보

--- tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/domain/water_place/RescueSupplyRepository.java
+++ tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/domain/water_place/RescueSupplyRepository.java
@@ -2,5 +2,9 @@ package kr.ac.kumoh.illdang100.tovalley.domain.water_place;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RescueSupplyRepository extends JpaRepository<RescueSupply, Long> {
+
+    Optional<RescueSupply> findByWaterPlace_Id(Long waterPlaceId);
 }
