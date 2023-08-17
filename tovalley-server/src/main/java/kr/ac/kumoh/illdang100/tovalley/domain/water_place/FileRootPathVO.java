816825f89ebc62299f2da9b1696cb961099c3262
Jimin <wl2258@naver.com>
2023-08-17 10:55:28 +0900

    Feat: FileRootPathVO 생성

---
+++ tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/domain/water_place/FileRootPathVO.java
@@ -0,0 +1,8 @@
+package kr.ac.kumoh.illdang100.tovalley.domain.water_place;
+
+public interface FileRootPathVO {
+
+    public static final String MEMBER_PATH = "member";
+    public static final String REVIEW_PATH = "review";
+    public static final String WATER_PLACE_PATH = "waterPlace";
+}
