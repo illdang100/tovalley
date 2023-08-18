c4d21f808c3c1e2f763bb5dbac8c091996c4e57a
Jimin <wl2258@naver.com>
2023-08-18 23:20:50 +0900

    Update: RetrieveWaterPlacesCondition 필드 추가

--- tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/dto/water_place/WaterPlaceReqDto.java
+++ tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/dto/water_place/WaterPlaceReqDto.java
@@ -2,12 +2,18 @@ package kr.ac.kumoh.illdang100.tovalley.dto.water_place;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import reactor.util.annotation.Nullable;
+import javax.validation.constraints.NotNull;
 
 public class WaterPlaceReqDto {
 
     @AllArgsConstructor
     @Data
     public static class RetrieveWaterPlacesCondition {
+        @NotNull
+        private String region;
 
+        @Nullable
+        private String searchWord;
     }
 }
