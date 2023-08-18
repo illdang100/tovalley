efbc842064b806a7faf76d883acd6f5b5bcc2bc9
Jimin <wl2258@naver.com>
2023-08-19 00:32:15 +0900

    Update: RetrieveWaterPlacesDto 필드 추가

--- tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/dto/water_place/WaterPlaceRespDto.java
+++ tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/dto/water_place/WaterPlaceRespDto.java
@@ -2,19 +2,21 @@ package kr.ac.kumoh.illdang100.tovalley.dto.water_place;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
 
 public class WaterPlaceRespDto {
 
     @AllArgsConstructor
     @Data
     public static class RetrieveWaterPlacesDto {
-
-        // 물놀이 장소 pk
-        // 물놀이 장소 이름
-        // town 정보(null일 수 있음)
-        // 평점
-        // 리뷰수
-        // managementType 정보 (일반지역, 중점관리 지역)
-        // waterPlaceCategory 정보 (일반지역, 중점관리지역)
+        private Long waterPlaceId; // 물놀이 장소 pk
+        private String waterPlaceName; // 물놀이 장소 이름
+        private String town; // town 정보 (null일 수 있음)
+        private Double waterPlaceRating; // 평점
+        private Integer waterPlaceReviewCnt; // 리뷰수
+        private String managementType; // managementType 정보 (일반지역, 중점관리 지역)
+        private String waterPlaceCategory; // waterPlaceCategory 정보 (하천, 계곡)
     }
 }
