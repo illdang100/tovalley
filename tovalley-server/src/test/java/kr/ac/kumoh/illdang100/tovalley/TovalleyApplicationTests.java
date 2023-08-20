97c4d4ddfc9ffd0b8598cd2181c04cd7b26146cf
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-08-20 16:09:35 +0900

    Fix: TovalleyApplicationTests 실행시 발생하는 버그 수정
    TovalleyApplicationTests 코드가 실행될 때마다 application-dev.yml 파일이 동작해서 에러가 발생하는 문제 해결

--- tovalley-server/src/test/java/kr/ac/kumoh/illdang100/tovalley/TovalleyApplicationTests.java
+++ tovalley-server/src/test/java/kr/ac/kumoh/illdang100/tovalley/TovalleyApplicationTests.java
@@ -2,7 +2,10 @@ package kr.ac.kumoh.illdang100.tovalley;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+
+@ActiveProfiles("test")
 @SpringBootTest
 class TovalleyApplicationTests {
 
