bc153aba714472f29ebb692be59d2ee22475c961
Jimin <wl2258@naver.com>
2023-08-21 19:05:16 +0900

    Feat: MemberReposiotry findByUsername() 추가

--- tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/domain/member/MemberRepository.java
+++ tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/domain/member/MemberRepository.java
@@ -2,5 +2,9 @@ package kr.ac.kumoh.illdang100.tovalley.domain.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByUsername(String username);
 }
