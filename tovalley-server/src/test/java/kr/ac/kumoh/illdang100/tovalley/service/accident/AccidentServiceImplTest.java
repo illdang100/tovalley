7813b5a778c89a9ec371b92ffdfc6cb96219d0fc
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-08-22 16:36:50 +0900

    Test: 여행 일정 추가 Service 코드 테스트 및 domain 테스트 디렉터리 위치 이동

--- tovalley-server/src/test/java/kr/ac/kumoh/illdang100/tovalley/service/accident/AccidentServiceImplTest.java
+++ tovalley-server/src/test/java/kr/ac/kumoh/illdang100/tovalley/service/accident/AccidentServiceImplTest.java
@@ -1,4 +1,4 @@
-package kr.ac.kumoh.illdang100.tovalley.service.domain.accident;
+package kr.ac.kumoh.illdang100.tovalley.service.accident;
 
 import kr.ac.kumoh.illdang100.tovalley.domain.accident.Accident;
 import kr.ac.kumoh.illdang100.tovalley.domain.accident.AccidentEnum;
