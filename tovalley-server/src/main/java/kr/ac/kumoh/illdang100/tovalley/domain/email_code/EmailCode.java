f3f28460eb140be85dd8597fb1624dd8b4551d95
Jimin <wl2258@naver.com>
2023-08-28 01:00:46 +0900

    Feat: EmailCode 생성

---
+++ tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/domain/email_code/EmailCode.java
@@ -0,0 +1,33 @@
+package kr.ac.kumoh.illdang100.tovalley.domain.email_code;
+
+import kr.ac.kumoh.illdang100.tovalley.domain.BaseTimeEntity;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class EmailCode {
+
+    @Id
+    @Column(name = "email_code_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(nullable = false, length = 25)
+    private String email;
+
+    @Column(nullable = false, length = 7)
+    private String verifyCode;
+
+    @Column(nullable = false)
+    private EmailCodeStatusEnum emailCodeStatus;
+
+    public void updateEmailCodeStatus(EmailCodeStatusEnum emailCodeStatus) {
+        this.emailCodeStatus = emailCodeStatus;
+    }
+}
