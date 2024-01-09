8180459c5780971c33e8a7f5e642de901dbe858d
jimin <wl2258@naver.com>
2024-01-09 23:33:52 +0900

    CH-11 사용자 권한 변환 페이지

---
+++ tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/dto/admin/AdminChangeRoleReqDto.java
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.illdang100.tovalley.dto.admin;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AdminChangeRoleReqDto {
+}
