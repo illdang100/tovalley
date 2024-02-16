3de360e287915dbf6cd0267e60c44c133e6847bd
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-02-17 00:37:23 +0900

    move: Message 클래스 to domain.chat 디렉토리

--- tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/domain/chat/Message.java
+++ tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/domain/chat/Message.java
@@ -1,4 +1,4 @@
-package kr.ac.kumoh.illdang100.tovalley.kafka;
+package kr.ac.kumoh.illdang100.tovalley.domain.chat;
 
 import java.io.Serializable;
 import javax.validation.constraints.NotNull;
@@ -12,8 +12,6 @@ import lombok.ToString;
 @Getter
 @ToString
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 // Kafka를 통해 전송되는 메시지의 데이터 구조
 /*
 메시지는 Producer에서 생성되어 Kafka Broker로 전송되고, 이후 Consumer가 해당 메시지를 받아 처리하게 된다.
@@ -23,17 +21,14 @@ public class Message implements Serializable {
 
     private String id;
 
-    @NotNull
     private Long chatRoomId;
 
-    @NotNull
     private Long senderId;
 
-    @NotNull
     private String content;
 
     private String createdAt;
-    private Integer readCount;
+//    private Integer readCount;
 
 //    @NotNull
 //    private String contentType;
@@ -41,6 +36,17 @@ public class Message implements Serializable {
 //    private String senderName;
 //    private Integer senderNo;
 //    private String senderEmail;
+
+    public Message() {
+    }
+
+    public Message(String id, Long chatRoomId, Long senderId, String content, String createdAt) {
+        this.id = id;
+        this.chatRoomId = chatRoomId;
+        this.senderId = senderId;
+        this.content = content;
+        this.createdAt = createdAt;
+    }
 
     public void setId(String id) {
         this.id = id;
@@ -60,7 +66,7 @@ public class Message implements Serializable {
                 .senderId(senderId)
                 .content(content)
                 .createdAt(createdAt)
-                .readCount(readCount)
+//                .readCount(readCount)
                 .build();
     }
 }
