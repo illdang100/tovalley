963e6ced50ea744d58231cb0e1c9f1c54b738e06
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-02-20 23:31:19 +0900

    feat: ChatUtil 필드 추가

--- tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/util/ChatUtil.java
+++ tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/util/ChatUtil.java
@@ -1,13 +1,20 @@
 package kr.ac.kumoh.illdang100.tovalley.util;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 public class ChatUtil {
 
     public static final String MEMBER_ID = "memberId";
+    public static final String SUBSCRIPTIONS = "subscriptions";
+    public static final int MAX_PARTICIPANTS_PER_CHATROOM = 2;
 
     public static LocalDateTime convertZdtStringToLocalDateTime(String zdtString) {
+
+        // ZonedDateTime zdt = ZonedDateTime.parse(zdtString);
+        // ZonedDateTime kstZdt = zdt.withZoneSameInstant(ZoneId.of("Asia/Seoul"));
+        // return kstZdt.toLocalDateTime();
 
         ZonedDateTime zdt = ZonedDateTime.parse(zdtString);
         return zdt.toLocalDateTime();
