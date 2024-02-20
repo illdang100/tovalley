5df086c5579fb443c5c96368b2b669c97e4c2449
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-02-20 22:57:24 +0900

    refactor: ChatRoomRepository 메서드 리팩토링

--- tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/domain/chat/ChatRoomRepository.java
+++ tovalley-server/src/main/java/kr/ac/kumoh/illdang100/tovalley/domain/chat/ChatRoomRepository.java
@@ -6,8 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long>, ChatRoomRepositoryCustom {
-    Optional<ChatRoom> findBySenderIdAndRecipientNickname(Long senderId, String recipientNick);
+    @Query("SELECT c FROM ChatRoom c WHERE " +
+            "(c.sender.id = :memberId AND c.recipient.nickname = :otherMemberNickname) OR " +
+            "(c.recipient.id = :memberId AND c.sender.nickname = :otherMemberNickname)")
+    Optional<ChatRoom> findByMemberIdAndOtherMemberNickname(@Param("memberId") Long memberId, @Param("otherMemberNickname") String otherMemberNickname);
 
-    @Query("select cr from ChatRoom cr join cr.sender s join cr.recipient r where cr.id = :chatRoomId and s.id = :memberId or r.id = :memberId")
+    @Query("select cr from ChatRoom cr join cr.sender s join cr.recipient r where cr.id = :chatRoomId and (s.id = :memberId or r.id = :memberId)")
     Optional<ChatRoom> findByIdAndMemberId(@Param("chatRoomId")Long chatRoomId, @Param("memberId")Long memberId);
+
+    @Query("SELECT cr FROM ChatRoom cr JOIN FETCH cr.sender s JOIN FETCH cr.recipient r WHERE cr.id = :id")
+    Optional<ChatRoom> findByIdWithMembers(@Param("id") Long id);
 }
