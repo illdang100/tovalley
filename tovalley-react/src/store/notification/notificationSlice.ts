c05e3ce283aaa2f762246260615d774d10353f97
yunzung <yunej27@gmail.com>
2024-03-03 00:22:40 +0900

    feat: 채팅 서버 연결 전 작업

--- tovalley-react/src/store/notification/notificationSlice.ts
+++ tovalley-react/src/store/notification/notificationSlice.ts
@@ -3,14 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const notificationSlice = createSlice({
   name: "notification",
   initialState: {
-    value: {
-      chatRoomId: 1,
-      recipientId: "test",
-      senderNick: 2,
-      createdDate: "2024-02-23T01:24:20.913397",
-      content: "hihi",
-      notificationType: "CHAT",
-    },
+    value: null,
   },
   reducers: {
     setNotification(state, action) {
